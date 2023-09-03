0eb464b46caec3b177da8460f807c3a1ac715e8b
Lucia Ariadna Fernandez <72044865+fernandezlucia@users.noreply.github.com>
2023-09-03 17:18:30 -0300

    paso sendero.cpp

--- TP1/Backtracking/sendero.cpp
+++ TP1/Backtracking/sendero.cpp
@@ -1,141 +1,132 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
-typedef vector<vector<int>> Tablero;
+#define L 'L'
+#define I 'I'
+#define CROSS '+'
+#define EMPTY '#'
 
-vector<int> longitudes;
-int cantPiezas = 7;
-enum Formas {UpRight, UpLeft, DownLeft, DownRight, Vertical, Horizontal, Cross, Empty};
+typedef vector<vector<char>> Tablero;
 
-int rotar(int pieza){
-    //es una L o una L rotada
-    if(pieza % cantPiezas < 4)
-        return (pieza + 1) % 4;
-    else 
-        return (++pieza % 2) + 4; // es una i o una i rotada
+int maxLen;
+int minLen;
+int n;
+int m;
+
+Tablero tablero;
+
+enum Direccion {Arriba, Izquierda, Abajo, Derecha};
+
+bool enRango(int i, int j){
+    return (i < tablero.size() && i >= 0) && (j < tablero[0].size() && j >= 0);
 }
 
-bool esSenderoValido(Tablero& tablero, int& longitud){
-    int camino = 0;
-    longitud = camino;
-    return true;
+void siguientePosicion(int prev_i, int prev_j, int direccion, int& sig_i, int& sig_j){
+    sig_i = prev_i;
+    sig_j = prev_j;
+    
+    switch(direccion){
+        case Arriba:
+            --sig_i;
+            break;
+        case Abajo:
+            ++sig_i;
+            break;
+        case Derecha:
+            ++sig_j;
+            break;
+        case Izquierda:
+            --sig_j;
+            break;
+    }
 }
 
-void printT(Tablero& tablero){
-    cout << "---------------" << endl;
-    for(int i = 0; i < tablero.size(); i ++){
-        for(int j = 0; j < tablero[i].size(); j++){
-            cout << tablero[i][j] << " ";
-        }
-        cout << "\n";
-    }
-            cout << "---------------" << endl;
-}
-int cant = 0;
+void haySendero(vector<vector<bool> >& visitado, int i, int j, int direccion, int longitud){
+    //caso base
+    if(!enRango(i,j))
+        return;
 
-bool haySendero(Tablero& tablero, int i, int j){
-    int n = tablero.size();
-    int m = tablero[0].size();
-    int pieza = tablero[i][j];
-    int aux = tablero[i][j];
-    bool hay = false;
-    int rotaciones = 0;
-    int sig_i;
-    int sig_j;
+    if(visitado[i][j])
+        return;
 
-    if(j == m-1){
-        sig_i = (i+1)%n;
-        sig_j = 0;
-    }else{
-        sig_i = i;
-        sig_j = (j+1)%m;
+    if(tablero[tablero.size()-1][tablero[0].size()-1] == EMPTY)
+        return;
+
+    if(i == n - 1 && j == m - 1){
+        minLen = min(minLen, longitud);
+        maxLen = max(maxLen, longitud);
+        return;
     }
 
-    // caso base
-    if(i == n-1 && j == m-1){
-        printT(tablero); 
-          cant++;
-          cout << cant << endl;
-        return hay;
-    }
-
+    visitado[i][j] = true;
+    
+    int sig_i, sig_j, paridad;
+    //caso recursivo
     switch(tablero[i][j]){
-        case UpRight:
-            rotaciones = 3;
-            break;
-        case Vertical:
-            rotaciones = 1;
-            break;
-        default:
-            rotaciones = 0;
-            break;
-    }
-
-    bool acum = true;
-    for(int k = 0; k <= rotaciones; k++){
-        //tablero[i][j] = rotar(pieza); 
-        tablero[i][j] += 1;
-        acum =+ haySendero(tablero, sig_i, sig_j);
-    }
-    tablero[i][j] = aux;
-    return acum;
-
-    return hay;
- }
-
-
-/*
-    Indexar recibe un char y lo traduce al número correspondiente.
-    De este modo el dominio pasa de caracteres a números, y podemos encontrar maneras de representar todos los ángulos de rotación.
-*/
-
-int indexar(char c){
-    switch(c){
-        case 'L':
-            return UpRight;
-        case '+':
-            return Cross;
-        case '#':
-            return Empty;
-        case 'I':
-            return Vertical;
-    }
-}
-
-
-bool getInputFromFile(string file_path, vector<Tablero> &tableros) {
-    int c, n, m;
-    ifstream file (file_path);
-
-    if(!file.is_open()) {
-        return EXIT_FAILURE;
-    }
-
-    file >> c;
-
-    for(int i = 0; i < c; ++i) {
-        file >> n >> m;
-        Tablero current_table (n, vector<int> (m));
-
-        for(int j = 0; j < n; ++j){
-            for(int k = 0; k < m; ++k){
-                char temp;
-                file >> temp;
-                current_table[j][k] = indexar(temp);
+        case CROSS:
+            for(int dir = Arriba; dir < 4; dir++){
+                siguientePosicion(i, j, dir, sig_i, sig_j);
+                haySendero(visitado, sig_i, sig_j, dir, longitud + 1);
             }
-        }
+            break;
         
-        tableros.push_back(current_table);
+        case L:
+            paridad = (direccion + 1) % 2;
+            for(int dir = paridad; dir < 4; dir += 2){
+                siguientePosicion(i, j, dir, sig_i, sig_j);
+                haySendero(visitado, sig_i, sig_j, dir, longitud + 1);
+            }
+            break;
+        
+        case I:
+            siguientePosicion(i, j, direccion, sig_i, sig_j);
+            haySendero(visitado, sig_i, sig_j, direccion, longitud + 1);            
+            break;
+        
+        case EMPTY:
+            return;
+        
+        default:
+            return;
     }
 
-    file.close();
-
-    return EXIT_SUCCESS;
+    visitado[i][j] = false;
 }
 
+//bool getInputFromFile(string file_path, vector<Tablero> &tableros) {
+//    int c, n, m;
+//    ifstream file (file_path);
+//
+//    if(!file.is_open()) {
+//        return 0;
+//    }
+//
+//    file >> c;
+//
+//    for(int i = 0; i < c; ++i) {
+//        file >> n >> m;
+//        Tablero current_table (n, vector<char> (m));
+//
+//        for(int j = 0; j < n; ++j){
+//            string palabra;
+//            file >> palabra;
+//            for(int k = 0; k < palabra.size(); ++k){
+//                current_table[j][k] = palabra[k];
+//            }
+//        }
+//        
+//        tableros.push_back(current_table);
+//    }
+//
+//    file.close();
+//
+//    return 0;
+//}
+//
 bool getInputManual(vector<Tablero> &tableros) {
     int c, n, m;
     char pieza;
@@ -144,42 +135,58 @@ bool getInputManual(vector<Tablero> &tableros) {
 
     for(int i = 0; i < c; ++i) {
         cin >> n >> m;
-        Tablero current_table (n, vector<int> (m));
+        Tablero current_table (n, vector<char> (m));
 
         for(int j = 0; j < n; ++j){
             for(int k = 0; k < m; ++k){
-                char temp;
-                cin >> temp;
-                current_table[j][k] = indexar(temp);
+                cin >> current_table[j][k];
             }
         }
         
         tableros.push_back(current_table);
     }
     
-    return EXIT_SUCCESS;
+    return 0;
 }
-
 
 int main(int argc, char **argv) {
     vector<Tablero> tableros;
     bool failed_table_read;
-
+    
     
     if(argc > 1)
-        failed_table_read = getInputFromFile(argv[1], tableros);
+        failed_table_read = 0;//getInputFromFile(argv[1], tableros);
     else
         failed_table_read = getInputManual(tableros);
     
     if(failed_table_read)
-        return EXIT_FAILURE;
+        return 1;
 
     //por cada test llenamos las estructuras
 
     for(int i = 0; i < tableros.size(); ++i){
         //backtracking
-        haySendero(tableros[i], 0, 0);
+        int min, max;
+        tablero = tableros[i];
+        
+        minLen = INT_MAX;
+        maxLen = INT_MIN;
+
+        n = tableros[i].size();
+        m = tableros[i][0].size();
+
+        vector<vector<bool>> visitado (n, vector<bool>(m, false));
+
+        if(tablero[0][0] == I || tablero[0][0] == L){
+            haySendero(visitado, 0, 0,  Abajo, 0);
+        }
+        haySendero(visitado, 0, 0,  Derecha, 0);
+
+        if(minLen == INT_MAX && maxLen == INT_MIN)
+            printf("IMPOSIBLE\n");
+        else
+            printf("POSIBLE %d %d\n", minLen, maxLen);
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
