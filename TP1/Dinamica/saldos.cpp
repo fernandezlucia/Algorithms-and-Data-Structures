4720ea369fde399aa28c8909c4d4bb1c1ccf6563
Matias <matcozzani@gmail.com>
2023-09-07 21:44:56 -0300

    saldos casi terminado

--- TP1/Dinamica/saldos.cpp
+++ TP1/Dinamica/saldos.cpp
@@ -3,15 +3,15 @@
 #include <algorithm>
 
 using namespace std;
-#define INDEFINIDO -1
-int offset = 10 * 10 * 10 * 10 * 10 * 10;
-//i recorre el vector de xi
-//saldoAcumulado la suma acumulada hasta el paso i
+int offset;
+vector<int> valores = vector<int>(100, 0);
+vector<vector<int> > memo = vector<vector<int> >(101, vector<int>((2*100*100)+1, -1));
+vector<pair<bool, bool> > signos = vector<pair<bool, bool>>(100, make_pair(false,false));
 
-bool saldos(int i, int acumulado, int w, vector<int>& v, vector<vector<bool>>& memo, vector<pair<bool, bool>>& signos){
+bool saldos(int i, int acumulado, int w, vector<int>& v, vector<vector<int> >& memo, vector<pair<bool, bool> >& signos){
     if(i == 0)
         return acumulado == w;
-    if(!memo[i][acumulado+offset]){
+    if(memo[i][acumulado+offset] == -1){
         bool sumar = saldos(i-1, acumulado+v[i-1], w, v, memo, signos);
         bool restar = saldos(i-1, acumulado-v[i-1], w, v, memo, signos);
 
@@ -21,47 +21,49 @@ bool saldos(int i, int acumulado, int w, vector<int>& v, vector<vector<bool>>& m
         if(restar)
             signos[i-1].second = true;
         
-        memo[i][acumulado+offset] = sumar | restar;
+        memo[i][acumulado+offset] = sumar || restar;
     }
     return memo[i][acumulado+offset];
 }
 
 
-
 int main() {
 
-
-    /*size_t c, n, m = 0;
-    cin >> c >> n >> m;*/
-        
-    size_t casos = 0;
+    int casos = 0;
     cin >> casos;
     
     for(int caso = 0; caso < casos; caso++){
         int n, w;
         cin >> n;
         cin >> w;
-        vector<int> valores = vector<int>();
         for(int x = 0; x < n; x++){
             int val;
             cin >> val;
-            valores.push_back(val);
+            val = val / 100;
+            valores[x] = val;
         }
 
-        vector<vector<bool>> memo = vector<vector<bool>>(n+1, vector<bool>(2*offset*n, NULL));
-        vector<pair<bool, bool>> signos = vector<pair<bool, bool>>(n, make_pair(false, false));
-
+        offset = 100*n;
+        w = w / 100;
         saldos(n, 0, w, valores, memo, signos);
 
-        for(int i = 0; i < signos.size(); i++){
-            if(signos[i].first and !signos[i].second)
-                cout << "+";
-            else if(!signos[i].first and signos[i].second)
-                cout << "-";
-            else
-                cout << '?';
+        for (int i = 0; i < n; i++) {
+            cout << (signos[i].first && !signos[i].second ? "+" : (signos[i].second && !signos[i].first ? "-" : "?"));
         }
         cout << endl;
+
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < (2*offset)+1; j++){
+                memo[i][j] = -1;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            signos[i] = make_pair(false, false);
+        }
+        for(int i = 0; i < n; i++){
+            valores[i] = 0;
+        }
     }
 
     return 0;
