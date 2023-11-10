77d418959b51198e2fd4f6891e323f867fc8eaca
Matias <matcozzani@gmail.com>
2023-11-10 19:54:04 -0300

    salvataje terminado

--- TP3/Salvataje/salvataje.cpp
+++ TP3/Salvataje/salvataje.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <tuple>
 #include <limits.h>
+#include <queue>
 
 using namespace std;
 
@@ -16,107 +17,49 @@ using namespace std;
 
 //Todo caso de test debe tener al menos 1 tunel
 
-struct Tunel{
-    int inicio;
-    int fin;
-    int cubren;
+
+struct Arista{
+    int w;
+    int d;
 };
 
 int cant_salones;
-vector<Tunel> pasadizos;
-vector<Tunel> tuneles;
+vector<vector<Arista>> grafo;
 
-void printVectorTuplas(vector<Tunel> &pasadizos){
+/*void printVectorTuplas(vector<Tunel> &pasadizos){
     for(Tunel i : pasadizos){
         cout << "(" << i.inicio << ", " << i.fin << ", " << i.cubren << ")" << endl;
     }
-}
+}*/
 
-bool compareThirdElement(const Tunel& a, const Tunel& b) {
-    return a.cubren > b.cubren;
-}
+int dijkstra(int src) {
+    priority_queue<pair<int, int>, vector<pair<int,int>>> pq;
+    vector<int> dist(cant_salones*4, INT_MAX);
 
-bool compareSecondElement(const Tunel& a, const Tunel& b) {
-    return a.fin > b.fin;
-}
-void sortVectorOfTuplesByThirdElement(vector<Tunel>& tuples) {
-    stable_sort(tuples.begin(), tuples.end(), compareThirdElement);
-}
-
-void sortVectorOfTuplesBySecondElement(vector<Tunel>& tuples) {
-    stable_sort(tuples.begin(), tuples.end(), compareSecondElement);
-}
-
-bool esTunel(int u, int v, vector<Tunel>& tuneles){     //se fija si la arista vista es alguna de las que forman un pasadizo y cubren mayor cant. de murallas (lsa que se guardan en tuneles)
-    bool es_igual = false;
-
-    if(tuneles.size() != 0){
-        if  (u == tuneles[0].inicio && v == tuneles[0].fin ||
-             u == tuneles[1].inicio && v == tuneles[1].fin ||
-             u == tuneles[2].inicio && v == tuneles[2].fin ){
-                es_igual = true;
-            }
-    }
-
-    return es_igual;
-}
-
-void getMax(vector<Tunel> &pasadizos, vector<Tunel> &tuneles){ // busca las tres tuneles que cubran mayor cant de murallas y no se pisen
-    if (pasadizos.size() == 0)
-        return;
-    // ordenados de mayor a menor en cubrimiento
-    tuneles.push_back(pasadizos[0]);
-    int lastI = 0;
-    for (int i = 1; i < pasadizos.size(); i++){
-        if (pasadizos[i].inicio >= tuneles[0].fin || pasadizos[i].fin <= tuneles[0].inicio){
-            tuneles.push_back(pasadizos[i]);
-            lastI = i;
-            break;
-        }
-    }
-
-    for (int i = lastI; i < pasadizos.size(); i++){
-
-        if(pasadizos[i].fin > tuneles[0].inicio && pasadizos[i].inicio < tuneles[0].fin)
-            continue;
-        
-        if(pasadizos[i].inicio < tuneles[1].fin && pasadizos[i].inicio > tuneles[i].inicio)
-            continue;
-        
-        tuneles.push_back(pasadizos[i]);
-    }
-}
-
-int distanciaMinima(vector<int> dist, vector<bool> spt){
-    int min = INT_MAX, min_index;
- 
-    for (int v = 0; v < cant_salones; v++){
-        if (spt[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
-    }
- 
-    return min_index;
-}
-
-int dijkstra(vector<vector<int>> &grafo, int src, vector<Tunel>& tuneles){
-    vector<int> dist(cant_salones, INT_MAX);    //dist guarda la distancia mas corta del origen hasta i
-    vector<bool> spt(cant_salones, false);     //spt es true sii el nodo i esta incluido en shortest path tree o si ya se sabe la menor distancia del origen a i
- 
-    dist[src] = 0;
-
-    for (int count = 0; count < cant_salones; count++) {
-        int u = distanciaMinima(dist, spt);
-
-        spt[u] = true;
-        for (int v = 0; v < cant_salones; v++){
-            if (!spt[v] && grafo[u][v] && dist[u] != INT_MAX && dist[u] + grafo[u][v] < dist[v]){
-                dist[v] = dist[u] + grafo[u][v];
-            }
-        }
-    }
+    pq.push({0, src});
     
-    int res = dist[dist.size()-1] + 1;  // sumo 1 a la respuesta pq hay que considerar que entrar al primer salon (1er nodo) tambien tarda 1 min
-    return res;
+    dist[src] = 0;
+    
+    while(!pq.empty()) {
+        int we, dst;
+        tie(we, dst) = pq.top();
+        pq.pop();
+        
+        for(Arista e : grafo[dst]){
+            if(dist[e.d] > dist[dst] + e.w){
+                dist[e.d] = dist[dst] + e.w;
+                pq.push({dist[e.d], e.d});
+            }
+        }
+    }
+
+    vector<int> capas;
+    for(int k = 0; k <= 3; k++){
+        //printf("pusheo posicion: %d con valor: %d \n", (cant_salones-1)+(cant_salones*k), dist[(cant_salones-1)+(cant_salones*k)]);
+        capas.push_back(dist[(cant_salones-1)+(cant_salones*k)]);
+    }
+
+    return *min_element(capas.begin(), capas.end());
 }
 
 
@@ -126,39 +69,28 @@ int main(){
 
     for(int i = 0; i < c; i++){
         cin >> cant_salones >> cant_tuneles;
-        vector<vector<int>> grafo(cant_salones, vector<int>(cant_salones,0));
-
-        for(int k = 0; k < cant_salones-1; k++){
-            grafo[k][k+1] = 1;  // cada nodo (salon) tiene una arista dirigida al siguiente nodo con peso 1 (pq tarda 1 min en saltar la muralla)
-        }
-
+        grafo = vector<vector<Arista>>(cant_salones*4);
+        
         for(int j = 0; j < cant_tuneles; j++){
-            int inicio, fin;
-            cin >> inicio >> fin;
-            grafo[inicio-1][fin-1] = 0;
+            int inicio, destino;
+            cin >> inicio >> destino;
+            inicio--; destino--;
+            for(int k = 0; k <= 2; k++){
+                grafo[inicio+(cant_salones*k)].push_back({2, destino+(cant_salones*(k+1))});
+            };
+        }
+
+        for(int j = 0; j < cant_salones*4; j++){
+            if(j % cant_salones == cant_salones-1)
+                continue;
+
+            grafo[j].push_back({1, j+1});
             
-            int cubre = fin - inicio;
-            if(cubre > 2){
-                pasadizos.push_back({inicio-1, fin-1, cubre});
-            }
         }
-
-        sortVectorOfTuplesByThirdElement(pasadizos);
-        sortVectorOfTuplesBySecondElement(pasadizos);
-        //printVectorTuplas(pasadizos);
-        tuneles = vector<Tunel>();
-        getMax(pasadizos, tuneles);
-        for(Tunel t : tuneles){
-            grafo[t.inicio][t.fin] = 2;
-        }
-        //cout << "--------------------" << endl;
-        //printVectorTuplas(tuneles);
-        int res1 = dijkstra(grafo, 0, tuneles);
-
-        cout << res1 << endl;
-
-        tuneles.clear();
-        pasadizos.clear();
+        
+        int res1 = dijkstra(0);
+        cout << res1+1 << endl;
+        
     }
 
     return 0;
