62271ba997c47282be5d543a547ba040fafb65d0
Matias <matcozzani@gmail.com>
2023-12-04 17:10:18 -0300

    Distancias terminado

--- TP3/Distancias/distancias.cpp
+++ TP3/Distancias/distancias.cpp
@@ -10,13 +10,8 @@ using namespace std;
 
 int n;
 vector<vector<int>> aristas;
-bool imposible;
-
-// DIST DE CONEXION(u,v) = minima cant. de enlaces para ir de u a v
-// LATENCIA(u,v) = minima cant. de tiempo (en milisegundos > 0) que se tarde en ir de u a v
-
-// Hay q obtener la dist. de conexion entre todo nodo (u,v) sabiendo las latencias o si las latencias son incompatibles con la red
-// Dan la matriz de adyacencias con peso, hay q devolver la matriz de distancias? -> Algo floyd?
+vector<vector<bool>> aristas_orig;
+vector<vector<int>> matriz_orig;
 
 void printSolution(vector<vector<int>> &matriz_dist){
     for (int i = 0; i < n; i++) {
@@ -31,28 +26,52 @@ void printSolution(vector<vector<int>> &matriz_dist){
 	}
 }
 
-void floydWarshall(vector<vector<int>> &dist){
+bool floydWarshallCheck(vector<vector<int>> &dist){
 	int i, j, k;
-    
+    bool res = true;
 	for (k = 0; k < n; k++) {
 		for (i = 0; i < n; i++) {
 			for (j = 0; j < n; j++) {
-				if (dist[i][j] > (dist[i][k] + dist[k][j]) && (dist[k][j] != INF && dist[i][k] != INF)){
-                    if(dist[i][j] > dist[i][k] + dist[k][j]){
-                        cout << "IMPOSIBLE" << endl;
-                        imposible = true;
-                        return;
-                    }
-                }
-                if(dist[i][j] == (dist[i][k] + dist[k][j])){
-                    aristas[i][j] += 1;
-                }
+				if (dist[i][k] != INF && dist[k][j] != INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 			}
 		}
 	}
-    
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+            if(matriz_orig[i][j] != INF && dist[i][j] < matriz_orig[i][j]){
+                return false;
+            }
+		}
+	}  
+
+    for (k = 0; k < n; k++) {
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+                //if(aristas_orig[i][j]) continue;
+                if(k == i || k == j) continue;
+                if(i == j) aristas[i][j] = 0;
+				else if(dist[i][j] == dist[i][k] + dist[k][j]){
+                    aristas[i][j] = INF;
+			    }
+		    }
+	    }
+    }
+    return res;
 }
 
+void floydWarshall(vector<vector<int>> &d){
+    int i, j, k;
+	for (k = 0; k < n; k++) {
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				if (d[i][k] < INF && d[k][j] < INF)
+                    d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
+			}
+		}
+	}
+}
 
 int main(){
     int c;
@@ -60,20 +79,27 @@ int main(){
 
     for(int k = 0; k < c; k++){
         cin >> n;
-        vector<vector<int>> matriz_ady(n, vector<int>(n,0));
-        aristas = vector<vector<int>>(n, vector<int>(n, -1));
-        for(int i = 0; i < n-1; i++){
-            for(int j = 0; j < i+1; j++){
+        vector<vector<int>> matriz_ady(n, vector<int>(n, INF));
+        aristas = vector<vector<int>>(n, vector<int>(n, 0));
+        aristas_orig = vector<vector<bool>>(n, vector<bool>(n, false));
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
                 int temp;
                 cin >> temp;
-                matriz_ady[i+1][j] = temp;
-                matriz_ady[j][i+1] = temp;
+                matriz_ady[i][j] = temp;
+                matriz_ady[j][i] = temp;
+                aristas[i][j] = 1;
+                aristas[j][i] = 1;
             }
         }
-        imposible = false;
-        floydWarshall(matriz_ady);
 
-        if(!imposible){
+        matriz_orig = matriz_ady;
+
+        if(!floydWarshallCheck(matriz_ady)){
+            cout << "IMPOSIBLE" << endl;
+            continue;
+        }else{
+            floydWarshall(aristas);
             cout << "POSIBLE" << endl;
             printSolution(aristas);
         }
